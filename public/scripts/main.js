ae6dbdec2ad5456f2818aa86d9fb5f54a8997d1b
Maciej Krüger <mkg20001@gmail.com>
2020-09-16 11:28:22 +0200

    Initial parcel code

---
+++ public/scripts/main.js
@@ -0,0 +1,43 @@
+function goto(element, link) {
+  element.style.transition = '.1s'
+  element.style.color = 'white'
+  element.style.borderColor = 'white'
+  setTimeout(function () {
+    element.style.color = ''
+    element.style.borderColor = ''
+    setTimeout(function () {
+      element.style.color = 'white'
+      element.style.borderColor = 'white'
+      setTimeout(function () {
+        element.style.color = ''
+        element.style.borderColor = ''
+        setTimeout(function () {
+          element.style.transition = ''
+          if (link === '#') {
+            alert('Page not ready yet')
+          } else {
+            window.location.href = link
+          }
+        }, 100)
+      }, 100)
+    }, 100)
+  }, 100)
+}
+
+function interceptClickEvent(e) {
+  let href
+  var target = e.target || e.srcElement
+  if (target.tagName === 'A') {
+    href = target.getAttribute('href')
+
+    e.preventDefault()
+    goto(target, href)
+  }
+}
+
+// listen for link click events at the document level
+if (document.addEventListener) {
+  document.addEventListener('click', interceptClickEvent)
+} else if (document.attachEvent) {
+  document.attachEvent('onclick', interceptClickEvent)
+}
