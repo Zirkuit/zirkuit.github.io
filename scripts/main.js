299ecdb5dc375ff90d5f4582602a33b0cfa08d22
Baran Önen <baranonenn@gmail.com>
2020-09-18 12:09:11 +0300

    Add href

--- scripts/main.js
+++ scripts/main.js
@@ -24,23 +24,27 @@ function goto(element, link) {
     }, 100);
 }
 
-function select(element) {
-    element.style.transition = ".1s"
-    element.style.color = "white"
-    element.style.borderColor = "white"
-    setTimeout(function(){ 
-        element.style.color = ""
-        element.style.borderColor = ""
-        setTimeout(function(){ 
-            element.style.color = "white"
-            element.style.borderColor = "white"
-            setTimeout(function(){ 
-                element.style.color = ""
-                element.style.borderColor = ""
-                setTimeout(function(){ 
-                    element.style.transition = ""
-                }, 100);
-            }, 100);
-        }, 100);
-    }, 100);
-}
+function interceptClickEvent(e) {
+    let href
+    var target = e.target || e.srcElement
+  
+    while (target) {
+      if (target.tagName === 'A') {
+        href = target.getAttribute('href')
+  
+        e.preventDefault()
+        goto(target, href)
+  
+        break
+      }
+  
+      target = target.parentElement
+    }
+  }
+  
+  // listen for link click events at the document level
+  if (document.addEventListener) {
+    document.addEventListener('click', interceptClickEvent)
+  } else if (document.attachEvent) {
+    document.attachEvent('onclick', interceptClickEvent)
+  }
