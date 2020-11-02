3e128faf3547d8719b97f6d1b1bdef371030ab6f
Arda Çebi <ardacebi1@gmail.com>
2020-11-02 22:56:11 +0300

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,83 +1,47 @@
-'use strict'
-
-function goto (element, link) {
-  element.style.transition = '.1s'
-  element.style.color = 'white'
-  element.style.borderColor = 'white'
+function goto(element, link) {
+  element.style.transition = ".1s"
+  element.style.color = "white"
+  element.style.borderColor = "white"
   setTimeout(function () {
-    element.style.color = ''
-    element.style.borderColor = ''
+    element.style.color = ""
+    element.style.borderColor = ""
     setTimeout(function () {
-      element.style.color = 'white'
-      element.style.borderColor = 'white'
+      element.style.color = "white"
+      element.style.borderColor = "white"
       setTimeout(function () {
-        element.style.color = ''
-        element.style.borderColor = ''
+        element.style.color = ""
+        element.style.borderColor = ""
         setTimeout(function () {
-          element.style.transition = ''
+          element.style.transition = ""
           if (link === '#') {
             alert('Page not ready yet')
           } else {
             window.location.href = link
           }
-        }, 100)
-      }, 100)
-    }, 100)
-  }, 100)
-}
+        }, 100);
+      }, 100);
+    }, 100);
+  }, 100);
 
-function formSubmit (form) {
-  const formData = new FormData(form)
+  function interceptClickEvent(e) {
+    let href
+    var target = e.target || e.srcElement
 
-  formData.append('job', window.location.pathname.split('/')[2].replace(/-/gmi, ' '))
+    while (target) {
+      if (target.tagName === 'A') {
+        href = target.getAttribute('href')
 
-  window.fetch('https://forms.krüger.it/zirkuit', {
-    method: 'POST', // or 'PUT'
-    body: formData
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data)
-      alert('Successfully applied')
-      form.style += ';display:none'
-    })
-    .catch((error) => {
-      console.error('Error:', error)
-      alert('Failed to submit application')
-    })
-}
+        e.preventDefault()
+        goto(target, href)
 
-setTimeout(() => {
-  let form
-  if ((form = document.getElementsByClassName('applyform')[0])) {
-    form.onsubmit = (e) => {
-      e.preventDefault()
-      formSubmit(form)
-    }
-  }
-}, 1000)
+        break
+      }
 
-function interceptClickEvent (e) {
-  let href
-  var target = e.target || e.srcElement
+      target = target.parentElement
 
-  while (target) {
-    if (target.tagName === 'A') {
-      href = target.getAttribute('href')
-
-      e.preventDefault()
-      goto(target, href)
-
-      break
-    }
-
-    target = target.parentElement
-  }
-}
-
-// listen for link click events at the document level
-if (document.addEventListener) {
-  document.addEventListener('click', interceptClickEvent)
-} else if (document.attachEvent) {
-  document.attachEvent('onclick', interceptClickEvent)
-}
+      // listen for link click events at the document level
+      if (document.addEventListener) {
+        document.addEventListener('click', interceptClickEvent)
+      } else if (document.attachEvent) {
+        document.attachEvent('onclick', interceptClickEvent)
+      }
